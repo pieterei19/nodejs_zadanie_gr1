a11df615059a1a81bee3271c86dc39b1722b4d11
wolnyei19 <102586094+wolnyei19@users.noreply.github.com>
2023-04-13 13:36:26 +0200

    Update axios.js

--- web/js/axios.js
+++ web/js/axios.js
@@ -1,31 +1,24 @@
 const axios = require('axios');
 const mysql = require('mysql');
 
-// Tworzenie połączenia z bazą danych
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'zsti1',
-  password: 'EciePecie666!',
-  database: 'zsti1'
+    host: 'localhost',
+    user: 'zsti1',
+    password: 'EciePecie666!',
+    database: 'zsti1'
 });
 
-// Pobieranie danych z pliku JSON
-axios.get('index.js')
-  .then(response => {
-    // Parsowanie danych JSON
-    const jsonData = response.data;
-
-    // Wprowadzanie danych do bazy danych MySQL
-    jsonData.forEach((item) => {
-      connection.query('INSERT INTO dane SET ?', item, (err, res) => {
-        if (err) throw err;
-        console.log('Dane zostały dodane do bazy danych.');
-      });
+axios.get('http://imiki.pl/cf/')
+    .then(function (response) {
+        const data = response.data;
+        const sql = 'INSERT INTO dane (temp1, temp2, temp3, Temp4, humidity) VALUES (?, ?, ?, ?, ?)';
+        data.forEach(function (item) {
+            connection.query(sql, [item.temp1, item.temp2, item.temp3, item.Temp4, item.humidity], function (error, results, fields) {
+                if (error) throw error;
+                console.log('Data saved successfully!');
+            });
+        });
+    })
+    .catch(function (error) {
+        console.log(error);
     });
-
-    // Zamykanie połączenia z bazą danych
-    connection.end();
-  })
-  .catch(error => {
-    console.log(error);
-  });
