a8eb13cd822e36b7991afe10c807c73fbe2f2217
pieterei19 <102586217+pieterei19@users.noreply.github.com>
2023-04-13 13:21:46 +0200

    Update laczenie.php

--- laczenie.php
+++ laczenie.php
@@ -14,7 +14,6 @@ $sql = "SELECT * FROM users";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-  // Wyświetlenie wyników w tabeli HTML
   echo "<table>";
   while($row = mysqli_fetch_assoc($result)) {
     echo "<tr><td>" . $row["id"]. "</td><td>" . $row["name"]. "</td></tr>";
