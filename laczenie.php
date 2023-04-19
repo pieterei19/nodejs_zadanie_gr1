ede91c1178e5fa9f9a58776d376d17d219e0b11c
noconei19 <102586076+noconei19@users.noreply.github.com>
2023-04-19 21:36:41 +0200

    update laczenie.php

--- laczenie.php
+++ laczenie.php
@@ -1,7 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+   
+</body>
+</html>
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "zsti1";
+$password = "EciePecie666!";
 $dbname = "zsti1";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -10,13 +22,13 @@ if (!$conn) {
   die("Nieudane połączenie: " . mysqli_connect_error());
 }
 
-$sql = "SELECT * FROM users";
+$sql = "SELECT * FROM dane";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
   echo "<table>";
   while($row = mysqli_fetch_assoc($result)) {
-    echo "<tr><td>" . $row["id"]. "</td><td>" . $row["name"]. "</td></tr>";
+    echo "<tr><td>" . $row["temp1"]. "</td><td>" . $row["temp2"]. "</td><td>". $row["temp3"]. "</td><td>". $row["temp4"]. "</td><td>". $row["humidity"]. "</td><td>". $row["TimeStamp"]. "</td></tr>;
   }
   echo "</table>";
 } else {
